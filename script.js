49e9593a60724a6812381a39561c5cd1ffb4adc0
Andrew Ho <andrewho5296@gmail.com>
2022-06-10 10:49:38 -0500

    API is useless :(

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("countryInp");
 searchBtn.addEventListener("click", () => {
     let countryName = countryInp.value;
+    let finalURL = 'https://restcountries.com/v3.1/name/${countryName}?fullText=true';
+    console.log(finalURL);
 });
