c10feb367bdc9e735debfd6026bf6cb2d95c6d1a
Andrew Ho <andrewho5296@gmail.com>
2022-06-10 10:41:00 -0500

    Country Guide App Init

---
+++ script.js
@@ -0,0 +1,5 @@
+let searchBtn = document.getElementById("search-btn");
+let countryInp = document.getElementById("countryInp");
+searchBtn.addEventListener("click", () => {
+    let countryName = countryInp.value;
+});
